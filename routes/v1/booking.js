a719f61b9eb421e2baacc30f64ffd4e45fb0acc7
Yasiru Nilan <nilanyasiru@gmail.com>
2025-02-16 09:25:40 +0530

    updated api docs

--- routes/v1/booking.js
+++ routes/v1/booking.js
@@ -12,6 +12,71 @@ import authenticate from "../../middlewares/authenticator.js";
  */
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateBookingRequest:
+ *       type: object
+ *       properties:
+ *         eventId:
+ *           type: string
+ *     CreateBookingResponse:
+ *       type: object
+ *       properties:
+ *         booking:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: integer
+ *             eventId:
+ *               type: integer
+ *             userId:
+ *               type: integer
+ *             status:
+ *               type: string
+ *             createdAt:
+ *               type: string
+ *             updatedAt:
+ *               type: string
+ *     CancelBookingRequest:  
+ *       type: object
+ *       properties:
+ *         bookingId:
+ *           type: string
+ *     CancelBookingResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ */ 
+
+
+
+/**
+ * @swagger
+ * /booking/add:
+ *   post:
+ *     summary: Create a new booking
+ *     tags: [Booking]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateBookingRequest'
+ *     responses:
+ *       201:
+ *         description: The booking was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBookingResponse' 
+ *       500:
+ *         description: Some server error
+ */
 router.post(
   "/add",
   validate(bookingSchema.createBookingSchema),
@@ -19,6 +84,30 @@ router.post(
   bookingController.createBooking
 );
 
+/**
+ * @swagger
+ * /booking/cancel:
+ *   post:
+ *     summary: Cancel a booking
+ *     tags: [Booking]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CancelBookingRequest'
+ *     responses:
+ *       200:
+ *         description: The booking was successfully canceled 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CancelBookingResponse'
+ *       500:
+ *         description: Some server error
+ */ 
 router.post(
   "/cancel",
   validate(bookingSchema.cancelBookingSchema),
